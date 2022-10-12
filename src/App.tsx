179d9bed3a19703434ae260c6b2b2f8e57b923e3
DanielLinker <dima.linki@web.de>
2022-10-12 12:36:55 +0200

    New

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () =>{
+//function declaration
+function App (){
+    //Gibt was aus wichtig
+
+    //Gibt zurück
   return (
-    <div className="App">
-      <h1></h1>
+    <div className='Position'>
+        <GetStart />
+        <GetNumber />
+        <GetString />
     </div>
   );
 }
+
+function GetStart (){
+    return(
+        <div>
+            <div>Start</div>
+            <div>Start</div>
+            <div>Start</div>
+        </div>
+    );
+}
+
+function GetNumber () {
+    return (
+        <div>
+            <div>Number_1</div>
+            <div>Number_2</div>
+            <div>Number_3</div>
+        </div>
+    );
+}
+    function GetString (){
+        return(
+            <div>
+                <div>String</div>
+                <div>String</div>
+                <div>String</div>
+            </div>
+        );
+    }
+
 
 export default App;
